90d418bf89726099a77c5180467450246342e851
Chris Kujawa <ckujawa@chriskujawadevelopment.com>
2019-08-20 14:50:45 -0400

    changing up styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,56 +1,70 @@
-import React from 'react';
-import Link from 'gatsby-link';
-import styled from 'styled-components';
-import Navigator from './navigator';
+import React from 'react'
+import Link from 'gatsby-link'
+import styled from 'styled-components'
+import Navigator from './navigator'
 
-import logo from '../imgs/logo.svg';
+import logo from '../imgs/logo.svg'
 
 const HeaderWrapper = styled.header`
-      background: black;
-      height: 100px;
-      margin-bottom: 1.5rem;
-      position: relative;
+  background: black;
+  height: 100px;
+  position: relative;
 
-      h1{
-          position: absolute;
-          top: 15px;
-          left: 30px;
+  h1 {
+    position: absolute;
+    top: 15px;
+    left: 30px;
 
-        img{
-          height: 75px;
-        }
-      }
-`;
+    img {
+      height: 75px;
+    }
+  }
+`
 
 const HeaderContainer = styled.div`
-      margin: 0 auto;
-      max-width: 1280px;
-      padding: 1.45rem 1.0875rem;
-`;
+  margin: 0 auto;
+  max-width: 1280px;
+  padding: 1.45rem 1.0875rem;
+`
 
 const Header = ({ siteTitle }) => (
   <HeaderWrapper>
-      <h1>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          <img src={logo} alt="Chris Kujawa Web Development logo" />
-        </Link>
-      </h1>
-      <Navigator Bottom="15px" Right="10px"> 
-        <ul>
-          <li><a href="/" className="menu-item" id="home">Home</a></li>
-          <li><a href="/blog" className="menu-item" id="blog">Blog</a></li>
-          <li><a href="/work" className="menu-item" id="work">Work</a></li>
-          <li><a href="/contact" className="menu-item" id="contact">Contact</a></li>
-        </ul>
-      </Navigator>
-  </HeaderWrapper >
-
+    <h1>
+      <Link
+        to="/"
+        style={{
+          color: 'white',
+          textDecoration: 'none',
+        }}
+      >
+        <img src={logo} alt="Chris Kujawa Web Development logo" />
+      </Link>
+    </h1>
+    <Navigator Bottom="15px" Right="10px">
+      <ul>
+        <li>
+          <a href="/" className="menu-item" id="home">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="/blog" className="menu-item" id="blog">
+            Blog
+          </a>
+        </li>
+        <li>
+          <a href="/work" className="menu-item" id="work">
+            Work
+          </a>
+        </li>
+        <li>
+          <a href="/contact" className="menu-item" id="contact">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </Navigator>
+  </HeaderWrapper>
 )
 
 export default Header
