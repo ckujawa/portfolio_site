6d4d98622f5a473532f118997c12e9dc5a13a203
Chris Kujawa <ckujawa@chriskujawadevelopment.com>
2019-08-21 13:51:14 -0400

    make query use allFile  to filter by sourceInstanceName

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,11 +6,14 @@ import styled from 'styled-components'
 
 export const query = graphql`
   query IndexQuery {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      totalCount
-      edges {
-        node {
-          id
+    allFile(
+      filter: { sourceInstanceName: { eq: "content" } }
+      sort: { fields: childMarkdownRemark___frontmatter___date, order: DESC }
+    ) {
+      nodes {
+        id
+        childMarkdownRemark {
+          html
           frontmatter {
             title
             date(formatString: "MMM DD, YYYY")
@@ -21,6 +24,7 @@ export const query = graphql`
           }
         }
       }
+      totalCount
     }
   }
 `
@@ -33,18 +37,20 @@ const Teaser = styled.div`
 export default ({ data }) => (
   <Layout>
     <div>
-      {data.allMarkdownRemark.edges.map(({ node: post }) => {
-        const { frontmatter } = post
-        return (
-          <Teaser>
-            <h2>
-              <Link to={frontmatter.path}>{frontmatter.title}</Link>
-            </h2>
-            <h4>By: {frontmatter.author}</h4>
-            <h4>{frontmatter.date}</h4>
-            <p>{frontmatter.excerpt}</p>
-          </Teaser>
-        )
+      {data.allFile.nodes.map(({ id, childMarkdownRemark }) => {
+        {
+          const { frontmatter } = childMarkdownRemark
+          return (
+            <Teaser key={id}>
+              <h2>
+                <Link to={frontmatter.path}>{frontmatter.title}</Link>
+              </h2>
+              <h4>By: {frontmatter.author}</h4>
+              <h4>{frontmatter.date}</h4>
+              <p>{frontmatter.excerpt}</p>
+            </Teaser>
+          )
+        }
       })}
     </div>
   </Layout>
