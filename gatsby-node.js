6dc912b19d0af6edaceb6a7e5ed820fb1a8ab9a8
Chris Kujawa <ckujawa@chriskujawadevelopment.com>
2019-09-19 13:12:26 -0400

    change how blogPosts are handled in createPages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,68 +1,76 @@
-const path = require('path');
-const _ = require('lodash');
+const path = require('path')
+const _ = require('lodash')
 
 exports.createPages = ({ actions, graphql }) => {
-    const { createPage } = actions;
-    const blogPostTemplate = path.resolve(`src/templates/blogPost.js`);
-    const tagTemplate = path.resolve("src/templates/postsByTag.js");
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve(`src/templates/blogPost.js`)
+  const tagTemplate = path.resolve('src/templates/postsByTag.js')
 
-    return graphql(
-        `{
-            allMarkdownRemark{
-                edges{
-                    node{
-                        html
-                        id
-                        frontmatter{
-                            date
-                            path
-                            title
-                            excerpt
-                            tags
-                        }
-                    }
-                }
+  return graphql(
+    `
+      {
+        allFile(
+          filter: { sourceInstanceName: { eq: "content" } }
+          sort: {
+            fields: childMarkdownRemark___frontmatter___date
+            order: DESC
+          }
+        ) {
+          nodes {
+            id
+            childMarkdownRemark {
+              html
+              frontmatter {
+                title
+                date(formatString: "MMM DD, YYYY")
+                path
+                tags
+                excerpt
+                author
+              }
             }
-        }`
-    ).then(result => {
-        if (result.errors) {
-            return Promise.reject(result.errors);
+          }
         }
+      }
+    `
+  ).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
+    const posts = result.data.allFile.nodes
 
-        const posts = result.data.allMarkdownRemark.edges;
+    posts.forEach(({ childMarkdownRemark }, index) => {
+      createPage({
+        path: childMarkdownRemark.frontmatter.path,
+        component: blogPostTemplate,
+        context: {
+          prev: index === 0 ? null : posts[index - 1].node,
+          next: index + 1 > posts.length - 1 ? null : posts[index + 1].node,
+        },
+      })
+    })
 
-        posts.forEach(({ node }, index) => {
-            createPage({
-                path: node.frontmatter.path,
-                component: blogPostTemplate,
-                context: {
-                    prev: index === 0 ? null : posts[index - 1].node,
-                    next: index + 1 > posts.length - 1 ? null : posts[index + 1].node,
-                },
-            });
-        });
+    // Tag pages:
+    let tags = []
+    // Iterate through each post, putting all found tags into `tags`
+    _.each(posts, post => {
+      if (_.get(post, 'childMarkdownRemark.frontmatter.tags')) {
+        tags = tags.concat(post.childMarkdownRemark.frontmatter.tags)
+      }
+    })
 
-        // Tag pages:
-        let tags = [];
-        // Iterate through each post, putting all found tags into `tags`
-        _.each(posts, edge => {
-            if (_.get(edge, "node.frontmatter.tags")) {
-                tags = tags.concat(edge.node.frontmatter.tags)
-            }
-        });
+    // Eliminate duplicate tags
+    tags = _.uniq(tags)
 
-        // Eliminate duplicate tags
-        tags = _.uniq(tags);
-
-        // Make tag pages
-        tags.forEach(tag => {
-            createPage({
-                path: `/tags/${_.kebabCase(tag)}/`,
-                component: tagTemplate,
-                context: {
-                    tag,
-                },
-            });
-        });
-    });
+    // Make tag pages
+    tags.forEach(tag => {
+      createPage({
+        path: `/tags/${_.kebabCase(tag)}/`,
+        component: tagTemplate,
+        context: {
+          tag,
+        },
+      })
+    })
+  })
 }
--- src/templates/postsByTag.js
+++ src/templates/postsByTag.js
@@ -2,53 +2,59 @@ import React from 'react'
 import Link from 'gatsby-link'
 import { graphql } from 'gatsby'
 import PropTypes from 'prop-types'
-import Layout from "../components/layout"
+import Layout from '../components/layout'
+import styled from 'styled-components'
+
+const PostList = styled.ul`
+  margin-top: 1rem;
+  padding: 0.3rem;
+`
 
 const Tags = ({ pageContext, data }) => {
-    const { tag } = pageContext
-    const { edges, totalCount } = data.allMarkdownRemark
-    const tagHeader = `${totalCount} post${
-        totalCount === 1 ? "" : "s"
-        }    tagged with "${tag}"`
+  const { tag } = pageContext
+  const { edges, totalCount } = data.allMarkdownRemark
+  const tagHeader = `${totalCount} post${
+    totalCount === 1 ? '' : 's'
+  }    tagged with "${tag}"`
 
-    return (
-        <Layout>
-            <div>
-                <h1>{tagHeader}</h1>
-                <ul>
-                    {edges.map(({ node }) => {
-                        const { path, title } = node.frontmatter
-                        return (
-                            <li key={path}>
-                                <Link to={path}>{title}</Link>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </div>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <div>
+        <h1>{tagHeader}</h1>
+        <PostList>
+          {edges.map(({ node }) => {
+            const { path, title } = node.frontmatter
+            return (
+              <li key={path}>
+                <Link to={path}>{title}</Link>
+              </li>
+            )
+          })}
+        </PostList>
+      </div>
+    </Layout>
+  )
 }
 
 Tags.propTypes = {
-    pathContext: PropTypes.shape({
-        tag: PropTypes.string.isRequired,
+  pathContext: PropTypes.shape({
+    tag: PropTypes.string.isRequired,
+  }),
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      totalCount: PropTypes.number.isRequired,
+      edges: PropTypes.arrayOf(
+        PropTypes.shape({
+          node: PropTypes.shape({
+            frontmatter: PropTypes.shape({
+              path: PropTypes.string.isRequired,
+              title: PropTypes.string.isRequired,
+            }),
+          }),
+        }).isRequired
+      ),
     }),
-    data: PropTypes.shape({
-        allMarkdownRemark: PropTypes.shape({
-            totalCount: PropTypes.number.isRequired,
-            edges: PropTypes.arrayOf(
-                PropTypes.shape({
-                    node: PropTypes.shape({
-                        frontmatter: PropTypes.shape({
-                            path: PropTypes.string.isRequired,
-                            title: PropTypes.string.isRequired,
-                        }),
-                    }),
-                }).isRequired
-            ),
-        }),
-    }),
+  }),
 }
 
 export default Tags
