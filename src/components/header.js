d8e885aaa2c673e2ef08854dc06e88b9ea6a59a2
Chris Kujawa <ckujawa@chriskujawadevelopment.com>
2019-09-19 13:13:09 -0400

    fix errors being reported on console

--- src/components/header.js
+++ src/components/header.js
@@ -21,12 +21,6 @@ const HeaderWrapper = styled.header`
   }
 `
 
-const HeaderContainer = styled.div`
-  margin: 0 auto;
-  max-width: 1280px;
-  padding: 1.45rem 1.0875rem;
-`
-
 const Header = ({ siteTitle }) => (
   <HeaderWrapper>
     <h1>
--- src/components/navigator.js
+++ src/components/navigator.js
@@ -1,51 +1,51 @@
-import React from "react";
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 import { FaBars, FaTimes } from 'react-icons/fa'
-import {above} from '../utilities/breakpoints'
+import { above } from '../utilities/breakpoints'
 
 const StyledBurgerMenu = styled.div`
-    --menu-width: calc(.6 * 100vw);
-    overflow: hidden;
+  --menu-width: calc(0.6 * 100vw);
+  overflow: hidden;
 
-    nav{
-        margin: 0;
-        padding: 0;  
-        position: absolute;
-        width: var(--menu-width);
-        right: calc(var(--menu-width) * -1);
-    }
+  nav {
+    margin: 0;
+    padding: 0;
+    position: absolute;
+    width: var(--menu-width);
+    right: calc(var(--menu-width) * -1);
+  }
 
-    ul{
-        list-style: none;
-    }
+  ul {
+    list-style: none;
+  }
 
-    a {
-        color: black;
-    }
+  a {
+    color: black;
+  }
 
-    .menu_toggle {
-        font-size: 2em;
-        line-height: 2em;
-        position: absolute;
-        top: 20px;
-        right: 20px;
-        color: white;
-    }
+  .menu_toggle {
+    font-size: 2em;
+    line-height: 2em;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    color: white;
+  }
 
-    .menu_toggle:hover,
-    .menu_toggle:focus{
-        color: red;
-    }
+  .menu_toggle:hover,
+  .menu_toggle:focus {
+    color: red;
+  }
 
-    .menu-item{
-        margin: 0 10px;
-        font-family: Arimo sans-serif;
-        text-decoration: none;
-        font-size: 1.5em;
-        color: black;
-    }
+  .menu-item {
+    margin: 0 10px;
+    font-family: Arimo sans-serif;
+    text-decoration: none;
+    font-size: 1.5em;
+    color: black;
+  }
 
-    ${ above.med`
+  ${above.med`
 
         .menu_toggle{
             top: -9999px;
@@ -53,10 +53,11 @@ const StyledBurgerMenu = styled.div`
         }
         
         nav {
-            ${ props => props.Bottom ? `bottom: ${props.Bottom};` : 'bottom: auto'}
-            ${ props => props.Right ? `right: ${props.Right};` : 'right: auto'}
-            ${ props => props.Top ? `top: ${props.Top};` : 'top: auto'}
-            ${ props => props.Left ? `left: ${props.Left};` : 'left: auto'}
+            ${props =>
+              props.Bottom ? `bottom: ${props.Bottom};` : 'bottom: auto'}
+            ${props => (props.Right ? `right: ${props.Right};` : 'right: auto')}
+            ${props => (props.Top ? `top: ${props.Top};` : 'top: auto')}
+            ${props => (props.Left ? `left: ${props.Left};` : 'left: auto')}
             width: auto;
         }
 
@@ -70,21 +71,19 @@ const StyledBurgerMenu = styled.div`
         }
         
     `}
-`;
+`
 
-export default ({children, Top, Right, Bottom, Left}) => (
-    <StyledBurgerMenu Top={Top} Right={Right} Bottom={Bottom} Left={Left}>
-        <a href="#main-menu" className="menu_toggle">
-            <FaBars/>
-        </a>
-        <nav id="main-menu">
-            <a href="#main-menu-toggle" className="menu_close">
-                <FaTimes />
-            </a>
+export default ({ children, Top, Right, Bottom, Left }) => (
+  <StyledBurgerMenu Top={Top} Right={Right} Bottom={Bottom} Left={Left}>
+    <a href="#main-menu" className="menu_toggle">
+      <FaBars />
+    </a>
+    <nav id="main-menu">
+      <a href="#main-menu-toggle" className="menu_close">
+        <FaTimes />
+      </a>
 
-            {children}
-            
-            <a href="#main-menu-toggle" className="backdrop" hidden></a>
-        </nav>
-    </StyledBurgerMenu >
+      {children}
+    </nav>
+  </StyledBurgerMenu>
 )
